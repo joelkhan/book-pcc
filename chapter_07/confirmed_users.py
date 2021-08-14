911629e077ffad93f379aefe8e77c1da9ce665ce
joel khan of work dev <joel-work-dev@khanins.com.cn>
2021-08-14 09:57:35 +0800

    modify the code fomatter

--- chapter_07/confirmed_users.py
+++ chapter_07/confirmed_users.py
@@ -7,7 +7,6 @@ confirmed_users = []
 #  Move each verified user into the list of confirmed users.
 while unconfirmed_users:
     current_user = unconfirmed_users.pop()
-    
     print(f"Verifying user: {current_user.title()}")
     confirmed_users.append(current_user)
     
